180e1521b384be31eaad2137e989b9e67ea1493e
Bintang <animerevindo@gmail.com>
2023-05-26 17:43:20 +0700

    server-list

---
+++ scripts/location-list.js
@@ -0,0 +1,35 @@
+const locations = [
+  "Los Angelas, California",
+  "Dallas, Texas",
+  "London, United Kingdom",
+  "Tokyo, Japan",
+  "Seoul, South Korea",
+  "Jakarta, Indonesia",
+  "Purwokerto, Indonesia",
+  "Bangkok, Thailand",
+  "Singapore",
+  "Frankfurt, Germany",
+  "Sydney, Australia",
+  "Johor Baru, Malaysia",
+  "Shanghai, China",
+  "Mumbai, India",
+  "Paris, France",
+];
+
+const listUL = document.getElementById("location-list");
+console.log(listUL);
+
+locations.forEach((res) => {
+  const locationListLi = document.createElement("li");
+
+  const locationName = document.createElement("h3");
+  locationName.innerText = res;
+
+  const signal = document.createElement("img");
+  signal.src = "./imgs/svg/signal.svg";
+
+  locationListLi.appendChild(locationName);
+  locationListLi.appendChild(signal);
+
+  listUL.appendChild(locationListLi);
+});
