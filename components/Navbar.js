733ea7abd50e963f6ab4a504aab5a65c6c0fdae1
Bintang <animerevindo@gmail.com>
2023-06-02 17:19:48 +0700

    login

---
+++ components/Navbar.js
@@ -0,0 +1,11 @@
+function Navbar() {
+  return /*#__PURE__*/ _jsx("div", {
+    children: /*#__PURE__*/ _jsx("h1", {
+      children: "Hello, React!",
+    }),
+  });
+}
+ReactDOM.render(
+  /*#__PURE__*/ _jsx(Navbar, {}),
+  document.getElementById("root")
+);
